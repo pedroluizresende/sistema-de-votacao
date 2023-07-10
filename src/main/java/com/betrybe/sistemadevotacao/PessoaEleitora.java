52382d847dcad659d9ce77ee3def7e9c87d14436
Pedro Resende <pedroluizrnunez@gmail.com>
2023-07-10 15:31:35 -0300

    finaliza requisito 3

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,29 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    this.cpf = cpf;
+    super.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  @Override
+  public String getNome() {
+    return super.getNome();
+  }
+
+  @Override
+  public void setNome(String nome) {
+    super.setNome(nome);
+  }
 }
