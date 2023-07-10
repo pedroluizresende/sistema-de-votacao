f5af9b65583df5b11addce83943578c8fa584c22
Pedro Resende <pedroluizrnunez@gmail.com>
2023-07-10 15:25:59 -0300

    finaliza requisito 2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,48 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
 
+  private int numero;
+
+  private int votos;
+
+
+  /**
+   * Método construtor.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    super.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
+
+  @Override
+  public String getNome() {
+    return super.getNome();
+  }
+
+  @Override
+  public void setNome(String nome) {
+    super.setNome(nome);
+  }
 }
